9060efcf2642f4afbf98892b33611581b4f7f6a6
schmatteo <michal.skrzyszowski@hotmail.com>
2023-03-04 12:53:58 +0000

    Update rent/send.php to use db_conn.php

--- rent/send.php
+++ rent/send.php
@@ -1,13 +1,12 @@
-<?php  
-include('../db_conn.php');
+<?php
+include_once "../db_conn.php";
 
 if (isset($_POST['item']) && isset($_POST['borrower']) && isset($_POST['date']) && isset($_POST['type'])) {
 
 	function validate($data){
        $data = trim($data);
 	   $data = stripslashes($data);
-	   $data = htmlspecialchars($data);
-	   return $data;
+	   return htmlspecialchars($data);
 	}
 
 	$item = validate($_POST['item']);
@@ -17,21 +16,16 @@ if (isset($_POST['item']) && isset($_POST['borrower']) && isset($_POST['date']) 
 
 	if (empty($item) || empty($borrower) || empty($date) || empty($type)) {
 		echo "One or more fields were empty";
-	}else {
+	} else {
+        $mysqli = new mysqli(HOSTNAME, USERNAME, PASSWORD, "rentalsregistry");
+      
+        $mysqli->query("INSERT INTO rentals (item, borrower, date, type) VALUES ('$item', '$borrower', '$date', '$type')");
 
-		$sql = "INSERT INTO info(item, borrower, date, type) VALUES('$item', '$borrower', '$date', '$type')";
-		$res = mysqli_query($conn, $sql);
-
-		if ($res) {
-			mysqli_close($conn); 
-        	header("Location:index.html"); 
-        	exit;
-		}else {
-			echo "Cannot send the information";
-		}
+        $mysqli->close();
+        
+        header("Location:index.html"); 
 	}
 
-}else {
+} else {
 	echo("There has been an error");
 }
-?>
