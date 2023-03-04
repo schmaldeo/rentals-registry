2f1dec570f2d6e569aa5d8079e4f4f5fa2059bb1
schmatteo <michal.skrzyszowski@hotmail.com>
2023-03-04 11:48:33 +0000

    Save DB details to .json file rather than .php

--- setup.php
+++ setup.php
@@ -3,8 +3,7 @@ if (isset($_POST['hostname']) && isset($_POST['username'])) {
     function validate($data) {
         $data = trim($data);
         $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
+        return htmlspecialchars($data);
     }
     
     $hostname = validate($_POST['hostname']);
@@ -12,15 +11,24 @@ if (isset($_POST['hostname']) && isset($_POST['username'])) {
     $password = $_POST['pass'];
 
     if (empty($hostname) || empty($username)) {
-        echo "One or more required fields were empty";
-    } else {          
-        $file = fopen('db_conn.php', 'w') or die ('Cannot open the file');
-        $txt = "<?php\n\$hname = '$hostname';\n\$uname = '$username';\n\$password = '$password';\n\$conn = mysqli_connect(\$hname, \$uname, \$password);\nif (!\$conn) {\necho 'Connection failed!';\nexit();\n}";
+        die("One or more required fields were empty"); 
+    } else {
+        $file = fopen('db_details.json', 'w') or die ('Cannot open the file');
+        class ArrayValue implements JsonSerializable {
+          public function __construct(array $array) {
+            $this->array = $array;
+          }
+  
+          public function jsonSerialize() {
+            return $this->array;
+          }
+        }
+
+        $array = ['hostname' => $hostname, 'username' => $username, 'password' => $password];
+        
+        $txt = json_encode(new ArrayValue($array));
         fwrite($file, $txt);
         fclose($file);
-        header('Location: ./dbsetup.php?hname='.$hostname.'&uname='.$username.'&pw='.$password);
+//        header('Location: ./dbsetup.php?hname='.$hostname.'&uname='.$username.'&pw='.$password);
     }
-
-    
 }
-?>
